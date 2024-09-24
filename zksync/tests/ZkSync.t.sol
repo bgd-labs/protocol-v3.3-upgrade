abe39c601ee2bdd1e84497c07ccfa8295ca25305
Harsh Pandey <harshsatishpandey@gmail.com>
2024-09-24 20:22:25 +0530

    fix: test block for zksync

--- zksync/tests/ZkSync.t.sol
+++ zksync/tests/ZkSync.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import {UpgradeTest} from './UpgradeTest.t.sol';
 import {DeploymentLibrary} from '../scripts/Deploy.s.sol';
 
-contract ZkSyncTest is UpgradeTest('zksync', 44918736) {
+contract ZkSyncTest is UpgradeTest('zksync', 45006016) {
   function _getPayload() internal virtual override returns (address) {
     return DeploymentLibrary._deployZKSync();
   }
