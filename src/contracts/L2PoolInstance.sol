e4e974578d7398827372f70826a9039bc154d169
Lukas <lukasstrassel@googlemail.com>
2024-10-02 10:07:33 +0200

    fix: lincense update

--- src/contracts/L2PoolInstance.sol
+++ src/contracts/L2PoolInstance.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 import {L2Pool, IPoolAddressesProvider} from 'aave-v3-origin/contracts/protocol/pool/L2Pool.sol';
 import {PoolInstance3_2} from './PoolInstance.sol';
