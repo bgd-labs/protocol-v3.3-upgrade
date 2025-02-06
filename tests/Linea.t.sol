c8255b968dcbd1f6b04d999e4df980d07f3b22f3
Lukas <lukasstrassel@googlemail.com>
2025-02-06 11:28:16 +0100

    fix: e2e test linea

--- tests/Linea.t.sol
+++ tests/Linea.t.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.0;
 
 import {ProtocolV3TestBase, IPool, IPoolDataProvider, IPoolAddressesProvider, IERC20} from 'aave-helpers/src/ProtocolV3TestBase.sol';
+import {GovV3Helpers} from 'aave-helpers/src/GovV3Helpers.sol';
 import {UpgradeTest} from './UpgradeTest.t.sol';
 import {DeploymentLibrary} from '../scripts/Deploy.s.sol';
 import {Payloads} from './Payloads.sol';
 import {UpgradePayload} from '../src/contracts/UpgradePayload.sol';
 
-contract LineaTest is UpgradeTest('linea', 15423773) {
+contract LineaTest is UpgradeTest('linea', 15493477) {
   function _getPayload() internal virtual override returns (address) {
     return DeploymentLibrary._deployLinea();
   }
@@ -16,15 +17,15 @@ contract LineaTest is UpgradeTest('linea', 15423773) {
     return Payloads.LINEA;
   }
 
-  function test_diff() external virtual override{
+  function test_diff() external virtual override {
+    GovV3Helpers.executePayload(vm, 3);
     UpgradePayload payload = UpgradePayload(_getTestPayload());
     IPoolAddressesProvider addressesProvider = payload.POOL_ADDRESSES_PROVIDER();
     IPool pool = IPool(addressesProvider.getPool());
     defaultTest(
       string(abi.encodePacked(vm.toString(block.chainid), '_', vm.toString(address(pool)))),
       pool,
-      address(payload),
-      false // custom override to skip e2e given the pool is not active yet
+      address(payload)
     );
   }
 }
