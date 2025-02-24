f94450aa31b4bf37b74bd3a87eb79376f0403b93
Lukas <lukasstrassel@googlemail.com>
2025-02-24 15:04:19 +0100

    fix: remove more duplicates

--- diff.ts
+++ diff.ts
@@ -1,5 +1,5 @@
 import {execSync} from 'child_process';
-import {readdir, readdirSync, readFileSync, statSync, unlinkSync} from 'fs';
+import {readdirSync, readFileSync, statSync, unlinkSync} from 'fs';
 import path from 'path';
 import {Hex, getAddress, slice} from 'viem';
 
@@ -12,38 +12,34 @@ const directoryPath = path.join(__dirname, 'reports'); // Change 'your-folder' t
 const erc1967ImplSlot = '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc';
 
 // diff all the networks
-// readdir(directoryPath, (err, files) => {
-//   if (err) {
-//     return console.error('Unable to scan directory:', err);
-//   }
+const files = readdirSync(directoryPath);
 
-//   // Filter files ending with '_after'
-//   const filteredFiles = files.filter((file) => file.endsWith('_after.json'));
+// Filter files ending with '_after'
+const filteredFiles = files.filter((file) => file.endsWith('_after.json'));
 
-//   for (const file of filteredFiles) {
-//     const contentBefore = JSON.parse(
-//       readFileSync(`${directoryPath}/${file.replace('_after', '_before')}`, {encoding: 'utf8'})
-//     );
-//     const contentAfter = JSON.parse(readFileSync(`${directoryPath}/${file}`, {encoding: 'utf8'}));
-//     // diff slots that are not pure implementation slots (e.g. things on addresses provider)
-//     execSync(
-//       `npx @bgd-labs/cli codeDiff --address1 ${contentBefore.poolConfig.protocolDataProvider} --chainId1 ${contentBefore.chainId} --address2 ${contentAfter.poolConfig.protocolDataProvider} --chainId2 ${contentAfter.chainId} -o file`
-//     );
+for (const file of filteredFiles) {
+  const contentBefore = JSON.parse(
+    readFileSync(`${directoryPath}/${file.replace('_after', '_before')}`, {encoding: 'utf8'})
+  );
+  const contentAfter = JSON.parse(readFileSync(`${directoryPath}/${file}`, {encoding: 'utf8'}));
+  // diff slots that are not pure implementation slots (e.g. things on addresses provider)
+  execSync(
+    `npx @bgd-labs/cli codeDiff --address1 ${contentBefore.poolConfig.protocolDataProvider} --chainId1 ${contentBefore.chainId} --address2 ${contentAfter.poolConfig.protocolDataProvider} --chainId2 ${contentAfter.chainId} -o file`
+  );
 
-//     for (const contract of Object.keys(contentAfter.raw)) {
-//       const implSlot = contentAfter.raw[contract].stateDiff[erc1967ImplSlot];
-//       if (implSlot) {
-//         execSync(
-//           `npx @bgd-labs/cli codeDiff --address1 ${bytes32ToAddress(
-//             implSlot.previousValue
-//           )} --chainId1 ${contentBefore.chainId} --address2 ${bytes32ToAddress(
-//             implSlot.newValue
-//           )} --chainId2 ${contentAfter.chainId} -o file`
-//         );
-//       }
-//     }
-//   }
-// });
+  for (const contract of Object.keys(contentAfter.raw)) {
+    const implSlot = contentAfter.raw[contract].stateDiff[erc1967ImplSlot];
+    if (implSlot) {
+      execSync(
+        `npx @bgd-labs/cli codeDiff --address1 ${bytes32ToAddress(
+          implSlot.previousValue
+        )} --chainId1 ${contentBefore.chainId} --address2 ${bytes32ToAddress(
+          implSlot.newValue
+        )} --chainId2 ${contentAfter.chainId} -o file`
+      );
+    }
+  }
+}
 
 // now as the diffing is done, let's remove duplicates and generate a report
 // Function to read files recursively
