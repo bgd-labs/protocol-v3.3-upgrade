363f653db577a27388fd04ae68536a487cdf4d17
Lukas <lukasstrassel@googlemail.com>
2025-02-24 13:46:07 +0100

    diff impl changes as well

--- diff.ts
+++ diff.ts
@@ -1,9 +1,15 @@
 import {execSync} from 'child_process';
 import {readdir, readFileSync} from 'fs';
 import path from 'path';
+import {Hex, getAddress, slice} from 'viem';
+
+function bytes32ToAddress(bytes32: Hex) {
+  return getAddress(slice(bytes32, 12, 32));
+}
 
 // Set the target directory
 const directoryPath = path.join(__dirname, 'reports'); // Change 'your-folder' to your target folder
+const erc1967ImplSlot = '0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc';
 
 // Read all files from the directory
 readdir(directoryPath, (err, files) => {
@@ -23,5 +29,18 @@ readdir(directoryPath, (err, files) => {
     execSync(
       `npx @bgd-labs/cli codeDiff --address1 ${contentBefore.poolConfig.protocolDataProvider} --chainId1 ${contentBefore.chainId} --address2 ${contentAfter.poolConfig.protocolDataProvider} --chainId2 ${contentAfter.chainId} -o file`
     );
+
+    for (const contract of Object.keys(contentAfter.raw)) {
+      const implSlot = contentAfter.raw[contract].stateDiff[erc1967ImplSlot];
+      if (implSlot) {
+        execSync(
+          `npx @bgd-labs/cli codeDiff --address1 ${bytes32ToAddress(
+            implSlot.previousValue
+          )} --chainId1 ${contentBefore.chainId} --address2 ${bytes32ToAddress(
+            implSlot.newValue
+          )} --chainId2 ${contentAfter.chainId} -o file`
+        );
+      }
+    }
   }
 });
